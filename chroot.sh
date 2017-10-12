0add7f5c1251c310810f21d736989942516bfb6f
purplesquid <purplesquid@users.noreply.github.com>
2017-10-11 20:08:20 -0400

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -14,7 +14,7 @@ chrootsystem(){
         esac
     done
         
-    read -rp "Enter in the hostname of the computer (e.g archlinuxpc)" host
+    read -rp "Enter in the hostname of the computer (e.g archlinuxpc): " host
     echo $host > /etc/hostname
     
     echo -e "Creating initial ramdisk environment and setting up kernel modules for init\n"
@@ -24,12 +24,27 @@ chrootsystem(){
     read -rsp $'Press any key to set the root passwd...\n' -n1 key
     passwd
     echo
-    read -rsp $'Press any key to install the Syslinux bootloader...\n' -n1 key
-    echo -ne "y" | pacman -S syslinux gptfdisk
-    syslinux-install_update -i -a -m
+    
+    PS3='Enter a number to install Grub or Syslinux: '
+    timestandard=("Grub" "Syslinux")
+    select reply in "${timestandard[@]}"
+    do
+        case $reply in
+            "Grub")
+                echo -e "y" pacman -S grub efibootmgr
+                grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+                grub-mkconfig -o /boot/grub/grub.cfg
+                break;;     
+            "Syslinux")
+                echo -e "y" | pacman -S syslinux gptfdisk
+                syslinux-install_update -i -a -m
+                break;;
+        *) echo "invalid option";;
+        esac
+    done
     echo
     lsblk /dev/sda
-    read -rsp $'Add the root partition number after /dev/   For example -->  LABEL arch  APPEND root=/dev/rootpartition number goes here rw. Once you hit a key, the terminal will automatically switch to the file...\n' -n1 key
+    read -rsp $'Add the root partition number after /dev/   For example -->  LABEL arch  APPEND root=/dev/ROOT PARTITION NUMBER GOES HERE rw. Once you hit a key, the terminal will automatically switch to the file...\n' -n1 key
     nano /boot/syslinux/syslinux.cfg 
 }
 chrootsystem
