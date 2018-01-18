3b6e9384ee1945de1888ee5ba0b202797fd15b6c
purplesquid <purplesquid@users.noreply.github.com>
2018-01-17 22:37:13 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,45 +1,17 @@
-chrootsystem(){  
-    PS3='Enter a number for your time standard: '
-    timestandard=("localtime" "UTC")
-    select reply in "${timestandard[@]}"
-    do
-        case $reply in
-            "localtime")
-                hwclock --systohc --localtime
-                break;;     
-            "UTC")
-                hwclock --systohc --utc
-                break;;
-        *) echo "invalid option";;
-        esac
-    done
-        
-    read -rp "Enter in the hostname of the computer (e.g archlinuxpc): " host
-    echo $host > /etc/hostname
+hostName = "archpc"
+
+hwclock --systohc --localtime
+#Setting hostname
+echo $hostName > /etc/hostname
     
-    echo -e "Creating initial ramdisk environment and setting up kernel modules for init\n"
-    mkinitcpio -p linux 
+echo -e "Creating initial ramdisk environment and setting up kernel modules for init\n"
+mkinitcpio -p linux 
     
-    echo
-    read -rsp $'Press any key to set the root passwd...\n' -n1 key
-    passwd
-    echo
+echo
+read -rsp $'Press any key to set the root passwd...\n' -n1 key
+passwd
+echo
     
-    PS3='Enter 1 to install Syslinux or another key to install different boot loader: '
-    timestandard=("Syslinux")
-    select reply in "${timestandard[@]}"
-    do
-        case $reply in    
-            "Syslinux")
-                echo -e "y" | pacman -S syslinux gptfdisk
-                syslinux-install_update -i -a -m
-                echo
-                lsblk /dev/sda
-                read -rsp $'Add the root partition number after /dev/   For example -->  LABEL arch  APPEND root=/dev/(ROOT PARTITION NUMBER GOES HERE) rw. Once you hit a key, the terminal will automatically switch to the file...\n' -n1 key
-                nano /boot/syslinux/syslinux.cfg 
-                break;;
-        *) break;;
-        esac
-    done
-}
-chrootsystem
+pacman -S syslinux gptfdisk --noconfirm
+syslinux-install_update -i -a -m
+nano /boot/syslinux/syslinux.cfg
